6a0761965dd3f6e850e349654888c36b47a0fc1f
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-27 21:53:57 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,7 @@ require_relative "../lib/student.rb"
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
 
+desc 'drop into the Pry console'
+  task :console => :environment do
+  Pry.start
+end
